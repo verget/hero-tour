7e0ee327e97bacdd6e21214fdc0f7dd445cd7a4e
WilixLead <lead@w.wilix.ru>
2017-06-24 21:22:28 +0300

    toh-pt4

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,3 +1,11 @@
-/**
- * Created by root on 24.06.2017.
- */
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+}
